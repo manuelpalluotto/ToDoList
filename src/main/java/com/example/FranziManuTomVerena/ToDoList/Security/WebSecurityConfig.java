7fb616cb31e828822f747ca36ecc139a0847d576
Franziska Seidl <Franziska.Seidl@tn.mischok.academy>
2025-04-10 14:52:03 +0200

    securityConfig CORS

--- src/main/java/com/example/FranziManuTomVerena/ToDoList/Security/WebSecurityConfig.java
+++ src/main/java/com/example/FranziManuTomVerena/ToDoList/Security/WebSecurityConfig.java
@@ -1,4 +1,57 @@
 package com.example.FranziManuTomVerena.ToDoList.Security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll());
+
+    return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000")); //Erlaubt Anfragen nur vom React-Frontend
+        config.setAllowedMethods(List.of("GET", "POST"));
+        config.setAllowedHeaders(List.of("*")); //Erlaubt alle Header z.B. Authorization, usw.
+        config.setAllowCredentials(true); //Cookies oder Auth-Header dürfen gesetzt werden
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // CORS-Regeln für alle Pfade gültig
+
+
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {}
 }
